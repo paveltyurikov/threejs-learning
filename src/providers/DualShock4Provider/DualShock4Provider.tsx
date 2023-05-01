c767ffb2b4a7ce76316f300946ba123cc5446730
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2023-05-01 16:45:18 +0400

    Add DualShock4Provider

---
+++ src/providers/DualShock4Provider/DualShock4Provider.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { DualShock4 } from "webhid-ds4";
+import { DualShock4HIDContextProvider } from "./context";
+
+
+export type DualShock4ProviderProps = {
+  children: React.ReactNode;
+};
+
+const STYLES = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%,-50%)",
+} as const;
+
+const useDS4State = () => {
+  const [controllers, setState] = React.useState<DualShock4[]>([]);
+  const onClick = React.useCallback(async () => {
+    const ds4 = new DualShock4();
+    await ds4.init();
+    setState((currState) => {
+      const active = currState.filter((ds) => ds.device.open);
+      active.push(ds4);
+      return active;
+    });
+  }, []);
+  return [controllers, onClick];
+};
+const DualShock4Provider = ({ children }: DualShock4ProviderProps) => {
+  const [controllers, onClick] = useDS4State();
+  return (
+    <DualShock4HIDContextProvider value={controllers}>
+      {children}
+      {controllers.length > 0 ? null : (
+        <button
+          style={STYLES}
+          // @ts-ignore
+          onClick={onClick}
+        >
+          connect DualShock4
+        </button>
+      )}
+    </DualShock4HIDContextProvider>
+  );
+};
+
+export default DualShock4Provider;
